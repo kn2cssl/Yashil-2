eee9c5cb4fa9bebf9bcabd444a2083b41f81e3a2
amin <m.amin.kashi@gmail.com>
2015-07-10 20:31:00 +0430

    version1.1

--- base-FPGA/src/main.c
+++ base-FPGA/src/main.c
@@ -1,9 +1,9 @@
 /**
- * \file
- *
- * \brief Empty user application template
- *
- */
+* \file
+*
+* \brief Empty user application template
+*
+*/
 
 #include <asf.h>
 #define F_CPU 32000000UL
@@ -13,28 +13,21 @@
 #include <stdlib.h>
 
 
-void NRF_init (void) ;
-void data_transmission (void);
-void every1ms(void);
+void NRF_init ( void ) ;
+void data_transmission ( void ) ;
+void every1ms ( void ) ;
 void fpga_connection ( void ) ;
+void wireless_connection ( void ) ;
+void packing_data ( void ) ;
 
-unsigned char Buf_Rx_L[_Buffer_Size] ;
-char Buf_Tx_L[_Buffer_Size];
-char Address[_Address_Width] = { 0x11, 0x22, 0x33, 0x44, 0x55};
-int motor_num=0,test=0;
-uint32_t kck_time_dir,time_test,kck_time_chip,charge_time=0,kck_time_sw=0,charge_count=0;
-int free_wheel=0;
-int free_wheel_flag = 0;
-int adc_change = 0;
-int wireless_reset=0;
-float adc =0;
-int flg_dir=0, flg_chip=0, charge_flg=0,flg_sw=0,SW_TEST_flg=0;
-bool reset_setpoint_flag = true;
 
 //================================
-bool new_wireless_data = false ;
-bool connection_permission = false ;
-bool packing_pemission = false ;
+//bool new_wireless_data = false ;
+//bool connection_permission = false ;
+//bool packing_pemission = false ;
+
+enum Data_Flow {new_wireless_data , new_jyro_data , data_packing , communication , nothing };
+enum Data_Flow data;
 
 struct Robot_Data
 {
@@ -69,8 +62,21 @@ uint64_t send_packet0;
 uint64_t send_packet1;
 uint64_t receive_packet0;
 uint64_t receive_packet1;
+int counter_10us=0,counter_1ms=0,counter3=0;
 //================================
 
+unsigned char Buf_Rx_L[_Buffer_Size] ;
+char Buf_Tx_L[_Buffer_Size];
+char Address[_Address_Width] = { 0x11, 0x22, 0x33, 0x44, 0x55};
+int motor_num=0,test=0;
+uint32_t kck_time_dir,time_test,kck_time_chip,charge_time=0,kck_time_sw=0,charge_count=0;
+int free_wheel=0;
+int free_wheel_flag = 0;
+int adc_change = 0;
+int wireless_reset=0;
+float adc =0;
+int flg_dir=0, flg_chip=0, charge_flg=0,flg_sw=0,SW_TEST_flg=0;
+bool reset_setpoint_flag = true;
 struct wireless_DATA
 {
 	uint8_t RID;
@@ -84,7 +90,7 @@ struct wireless_DATA
 	signed int M3b;
 	uint8_t KCK;
 	uint8_t CHP;
-	uint8_t ASK;	
+	uint8_t ASK;
 }Robot_D;
 
 
@@ -98,7 +104,7 @@ struct Driver
 	float cur[4];
 	uint16_t count_H[4],count_L[4];
 	int cur_alarm[4];
-	};
+};
 struct Driver Driver;
 
 int change_ADC,flg_change=0;
@@ -128,23 +134,6 @@ int parity_calc(signed int data);
 
 int main (void)
 {
-	
-	Robot_D.RID=0;
-	Robot_D.M0a=1;
-	Robot_D.M0b=2;
-	Robot_D.M1a=3;
-	Robot_D.M1b=4;
-	Robot_D.M2a=0;
-	Robot_D.M2b=0;
-	Robot_D.M3a=0;
-	Robot_D.M3b=0;
-	Robot_D.KCK=0;
-	Robot_D.CHP=0;
-	Robot_D.ASK=0;
-	
-	
-
-
 	En_RC32M();
 
 	//Enable LowLevel & HighLevel Interrupts
@@ -161,82 +150,228 @@ int main (void)
 	//ADCA_init();
 	//ADCB_init();
 	
-	// Globally enable interrupts
-
-
+	//================================================================= Globally enable interrupts
 	//uint8_t temp;
 	//temp = WDT_PER_2KCLK_gc | (1 << WDT_ENABLE_bp) | (1 << WDT_CEN_bp);
 	//ccp_write_io((void *)&WDT.CTRL, temp);
 	//wdt_wait_while_busy();
 	
-	sei();
-
-
-
-
+	//// NRF Initialize
+	NRF_init () ;
 	
+	
+	sei();
+		
 	while(1)
-	  {  
-
-		if (connection_permission)
+	{
+		
+//================================================WIRELESS DATA
+		if (data == new_wireless_data)
 		{
-			
-			if (packing_pemission)
-			{
-				uint8_t check_sum0 = Robot.alphal + Robot.Wh + Robot.Wl + Robot.Vyh + Robot.Vyl + Robot.Vxh + Robot.Vxl ;
-				uint8_t check_sum1 = Robot.alphah + Robot.JWh + Robot.JWl + Robot.JVyh + Robot.JVyl + Robot.JVxh + Robot.JVxl ;
-				
-				send_packet0 = check_sum0<<56 | Robot.alphal<<48 |Robot.Wh<<40 | Robot.Wl<<32 | Robot.Vyh<<24 | Robot.Vyl<<16 | Robot.Vxh<<8 | Robot.Vxl ; //MSB of check_sum won't be sent
-				send_packet1 = check_sum1<<56 | Robot.alphah<<48 |Robot.JWh<<40 | Robot.JWl<<32 | Robot.JVyh<<24 | Robot.JVyl<<16 | Robot.JVxh<<8 | Robot.JVxl ; //MSB of check_sum won't be sent
-				
-				packing_pemission = false ;
-			}
-			
-
-			fpga_connection();				
-
-			
+			wireless_connection();
+			data = data_packing ;
 		}
-		else
+		_delay_us(1);
+//-------------------------------------------------------------
+
+//================================================JYRO DATA
+		//if (data == new_jyro_data)
+		//{
+			//data = data_packing ;
+		//}
+		//_delay_us(1);
+//-------------------------------------------------------------
+
+		if (data == data_packing)
 		{
-			
+			packing_data () ;
+			counter_10us = 0 ;
+			data = communication ;
 		}
 		
+		data = communication;
+		if (data == communication)
+		{
+			fpga_connection () ;
+		}
 		
+		if (data == nothing)
+		{
+			//counter_10us = 0;
+			//data = communication;
+		}
 
-
-	  }
-	   
+	}
+	
 }
 
-int counter_10us=0,counter_1ms=0,counter3=0;
 
- ISR(TCE1_OVF_vect)//10us
- {
-	 if (new_wireless_data==true)
-	 {
-		 counter_10us = 0;
-		 counter_1ms = 0;
-		 new_wireless_data = false;
-		 connection_permission = true;
-	 }
+ISR(PORTD_INT0_vect)////////////////////////////////////////PTX   IRQ Interrupt Pin
+{
+	data = new_wireless_data ;
+}
 
+
+
+ISR(TCE1_OVF_vect)//10us
+{
 	counter_10us++;
 	CLK_PORT.OUTTGL = CLK_PIN;
-	
-	if (counter_10us==100)//1ms
-	{
-		counter_1ms++;
-		counter_10us=0;
-	}
+	//
+	//if (counter_10us == 100)//1ms
+	//{
+		//
+		//counter_1ms ++ ;
+		//counter_10us = 0 ;
+				//LED_Red_PORT.OUTCLR = LED_Red_PIN_bm;
+				//LED_White_PORT.OUTCLR = LED_White_PIN_bm;
+	//}
+//
+	//if (counter_1ms == 1000)//1s
+	//{
+//
+		//counter3+=1;
+		//counter_1ms=0;
+		////LED_GR_PORT.OUTTGL = LED_Red_PIN_bm;
+	//}
+}
 
-	if (counter_1ms==1000)//1s
+
+void wireless_connection ( void )
+{
+	uint8_t status_L = NRF24L01_L_WriteReg(W_REGISTER | STATUSe, _TX_DS|_MAX_RT|_RX_DR);
+	if((status_L & _RX_DR) == _RX_DR)
 	{
-		counter3+=1;
-		counter_1ms=0;
-		LED_Red_PORT.OUTTGL = LED_Red_PIN_bm;				
+		LED_White_PORT.OUTSET = LED_White_PIN_bm;
+		wireless_reset=0;
+		wdt_reset();
+		//1) read payload through SPI,
+		NRF24L01_L_Read_RX_Buf(Buf_Rx_L, _Buffer_Size);
+		free_wheel=0 ;
+		if((Buf_Rx_L[0] == 0x0A && (RobotID < 3 || (RobotID<9 && RobotID>5)))|| (Buf_Rx_L[0] == 0xA0 && (RobotID > 8 || (RobotID<6 &&      RobotID>2))))
+		{
+			LED_Red_PORT.OUTSET = LED_Red_PIN_bm;
+			Robot_D.RID  = Buf_Rx_L[0];
+			Robot_D.M0a  = Buf_Rx_L[1+ RobotID%3 * 10];
+			Robot_D.M0b  = Buf_Rx_L[2+ RobotID%3 * 10];
+			Robot_D.M1a  = Buf_Rx_L[3+ RobotID%3 * 10];
+			Robot_D.M1b  = Buf_Rx_L[4+ RobotID%3 * 10];
+			Robot_D.M2a  = Buf_Rx_L[5+ RobotID%3 * 10];
+			Robot_D.M2b  = Buf_Rx_L[6+ RobotID%3 * 10];
+			Robot_D.M3a  = Buf_Rx_L[7+ RobotID%3 * 10];
+			Robot_D.M3b  = Buf_Rx_L[8+ RobotID%3 * 10];
+			Robot_D.KCK  = Buf_Rx_L[9+ RobotID%3 * 10];
+			Robot_D.CHP  = Buf_Rx_L[10+RobotID%3 * 10];
+			Robot_D.ASK  = Buf_Rx_L[31];//0b00000000
+			
+			if (Robot_D.ASK != Robot_Select)
+			{
+				Robot_Select = Robot_D.ASK;
+				if (Robot_Select == RobotID)
+				{
+					NRF24L01_L_WriteReg(W_REGISTER | EN_AA, 0x01);
+				}
+				else
+				{
+					NRF24L01_L_WriteReg(W_REGISTER | EN_AA, 0x00);
+				}
+			}
+			
+			if (Robot_D.ASK == RobotID)
+			{
+				data_transmission();
+			}
+
+		}
 	}
- }
+	
+	if ((status_L&_MAX_RT) == _MAX_RT)
+	{
+		NRF24L01_L_Flush_TX();
+	}
+}
+
+
+
+void NRF_init (void)
+{
+	NRF24L01_L_CE_LOW;       //disable transceiver modes
+	SPI_Init();
+
+	_delay_us(10);
+	_delay_ms(11);      //power on reset delay needs 10.3ms//amin changed 100ms to 11ms
+	NRF24L01_L_Clear_Interrupts();
+	NRF24L01_L_Flush_TX();
+	NRF24L01_L_Flush_RX();
+	NRF24L01_L_CE_LOW;
+	
+	
+	
+	if (RobotID < 6)
+	NRF24L01_L_Init_milad(_RX_MODE, _CH_1, _2Mbps, Address, _Address_Width, _Buffer_Size, RF_PWR_MAX);
+	else if(RobotID > 5)
+	NRF24L01_L_Init_milad(_RX_MODE, _CH_0, _2Mbps, Address, _Address_Width, _Buffer_Size, RF_PWR_MAX);
+	NRF24L01_L_WriteReg(W_REGISTER | DYNPD,0x01);
+	NRF24L01_L_WriteReg(W_REGISTER | FEATURE,0x06);
+
+	NRF24L01_L_CE_HIGH;
+	_delay_us(130);
+}
+
+
+
+void data_transmission (void)
+{
+	//transmitting data to wireless board/////////////////////////////////////////////////
+
+	//Test_Data[4] = adc*0.4761;//battery voltage
+	Test_Data[0] = (received_data[0] & 0x00ff) | ((received_data[1] & 0x00ff ) <<8);
+	Test_Data[4] = counter_1ms;
+	
+	
+	Buf_Tx_L[0]  = (Test_Data[0]>> 8) & 0xFF;//Robot_D.M0a;//	//drive test data
+	Buf_Tx_L[1]  = Test_Data[0] & 0xFF;//Robot_D.M0b;//			//drive test data
+	Buf_Tx_L[2]  = (Test_Data[1]>> 8) & 0xFF;//Robot_D.M1a;//	//drive test data
+	Buf_Tx_L[3]  = Test_Data[1] & 0xFF;	//Robot_D.M1b;//		//drive test data
+	Buf_Tx_L[4]  = (Test_Data[2]>> 8) & 0xFF;//Robot_D.M2a;//	//drive test data
+	Buf_Tx_L[5]  = Test_Data[2] & 0xFF;//Robot_D.M2b;//			//drive test data
+	Buf_Tx_L[6]  = (Test_Data[3]>> 8) & 0xFF;//Robot_D.M3a;//	//drive test data
+	Buf_Tx_L[7]  = Test_Data[3] & 0xFF;//Robot_D.M3b;//			//drive test data
+	Buf_Tx_L[8]  = (Test_Data[4]>> 8) & 0xFF;	// unused
+	Buf_Tx_L[9]  = Test_Data[4] & 0xFF;			// unused
+	Buf_Tx_L[10] = (Test_Data[5]>> 8) & 0xFF;// unused
+	Buf_Tx_L[11] = Test_Data[5] & 0xFF;			// unused
+	Buf_Tx_L[12] = (Test_Data[6]>> 8) & 0xFF;	// unused
+	Buf_Tx_L[13] = Test_Data[6] & 0xFF;			// unused
+	Buf_Tx_L[14] = (Test_Data[7]>> 8) & 0xFF;	// battery voltage
+	Buf_Tx_L[15] = Test_Data[7] & 0xFF;			// battery voltage
+	Buf_Tx_L[16] = adc/12;						//battery adc
+	
+
+	//LED_Red_PORT.OUTTGL = LED_Red_PIN_bm;
+	NRF24L01_L_Write_TX_Buf(Buf_Tx_L, _Buffer_Size);
+	//NRF24L01_L_RF_TX();
+}
+
+
+
+void packing_data ( void ) 
+{
+
+		//Robot.Vxl = 0xfa;
+		uint8_t check_sum0 = Robot.alphal + Robot.Wh + Robot.Wl + Robot.Vyh + Robot.Vyl + Robot.Vxh + Robot.Vxl ;
+		uint8_t check_sum1 = Robot.alphah + Robot.JWh + Robot.JWl + Robot.JVyh + Robot.JVyl + Robot.JVxh + Robot.JVxl ;
+					
+		send_packet0 = 72057628399779969;check_sum0<<56 | Robot.alphal<<48 |Robot.Wh<<40
+		| Robot.Wl<<32 | Robot.Vyh<<24 | Robot.Vyl<<16
+		| Robot.Vxh<<8 | Robot.Vxl ; //MSB of check_sum won't be sent
+
+		send_packet1 = 72057628399779969;check_sum1<<56 | Robot.alphah<<48 |Robot.JWh<<40
+		| Robot.JWl<<32 | Robot.JVyh<<24 | Robot.JVyl<<16
+		| Robot.JVxh<<8 | Robot.JVxl ; //MSB of check_sum won't be sent
+					
+}
+
 
 
 void fpga_connection ( void )
@@ -259,7 +394,7 @@ void fpga_connection ( void )
 		//nothing
 		break ;
 	}
-		
+	
 	if (counter_10us < 22 && counter_10us > 3)
 	{
 		if (counter_10us % 2 == 0 )
@@ -273,8 +408,8 @@ void fpga_connection ( void )
 			receive_packet0 = PORTX_IN | receive_packet0 ;
 		}
 	}
-		
-		
+	
+	
 	if (counter_10us > 21)
 	{
 		if (counter_10us % 2 == 0 )
@@ -292,7 +427,7 @@ void fpga_connection ( void )
 	if (counter_10us == 39)
 	{
 		//unpacking data from FPGA
-		for (int i =0 ; i ; i < 18 )
+		for (int i =0 ; i ; i < 16 )
 		{
 			if(i<9)
 			{
@@ -304,33 +439,33 @@ void fpga_connection ( void )
 				temp_data[i] = receive_packet1 & 0x000000FF ;
 				receive_packet1 = receive_packet1 >> 8 ;
 			}
-					 
+			
 			
 		}
 		
 		//generating check_sum
 		uint8_t check_sum_test0 = 0 ;
 		uint8_t check_sum_test1 = 0 ;
-		for (int i = 0 ; i++ ; i < 9)
+		for (int i = 0 ; i++ ; i < 7)
 		{
 			check_sum_test0 +=  temp_data[i] ;
 		}
 		
-		for (int i = 9 ; i++ ; i < 17)
+		for (int i = 8 ; i++ ; i < 15)
 		{
 			check_sum_test1 +=  temp_data[i] ;
 		}
 		
 		//saving checked data
-		if( ( (check_sum_test0 & 0x7F) == temp_data[8] ) && ( (check_sum_test1 & 0x7F) == temp_data[17] ))
+		if( ( (check_sum_test0 & 0x7F) == temp_data[7] ) && ( (check_sum_test1 & 0x7F) == temp_data[15] ))
 		{
-			for (int i = 9 ; i++ ; i < 17)
+			for (int i = 0 ; i++ ; i < 16)
 			{
 				received_data[i] = temp_data[i] ;
 			}
 		}
-
-
+		
+		data = nothing ;
 	}
 
 
